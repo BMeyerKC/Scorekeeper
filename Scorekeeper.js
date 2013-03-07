ac4330c14d2a5d4a230b53cd4f0fcfc2d09bf109
Brandon Meyer <bmeyerkc@gmail.com>
2013-03-07 00:17:21 -0600

    Binded the basic Player collection

--- Scorekeeper.js
+++ Scorekeeper.js
@@ -1,8 +1,34 @@
-if (Meteor.isClient) {
+Players = new Meteor.Collection("players");
+if( Meteor.isClient ) {
+  Meteor.startup( function( ) {
+    // code to run on client at startup
+  } );
+    var playerScoreAdd = function( player ) {
+      console.log("playerScoredAdd");
+      console.log( player );
+      Players.update( {_id: player._id}, {$set: {"score": player.score +1}} );
+    };
+    Template.players.player = function( ) {
+    return Players.find( );
+  };
+  Template.players.events = {
+    "click .playerScoreSubtract": function( event, template ) {
+      Players.update( {_id: this._id}, {$set: {"score": this.score -1}} );
+    },
+    "click .playerScoreAdd": function( event, template ) {
+      //Players.update( {_id: this._id}, {$set: {"score": this.score +1}} );
+    },
+    "mousedown .playerScoreAdd": function( event, template ) {
+      playerScoreAdd(this);
+      //Players.update( {_id: this._id}, {$set: {"score": this.score +1}} );
+    },
+    "mouseup .playerScoreAdd": function( event, template ) {
+      //clearTimeout( t );
+    }
+  };
 }
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
+if( Meteor.isServer ) {
+  Meteor.startup( function( ) {
     // code to run on server at startup
-  });
+  } );
 }
