01bcb0ec4a3a0dd0656071beb6ae4c37c48b6b68
Brandon Meyer <bmeyerkc@gmail.com>
2013-03-08 18:32:09 -0600

    Accelerated scoring

--- Scorekeeper.js
+++ Scorekeeper.js
@@ -1,34 +1,46 @@
 Players = new Meteor.Collection("players");
-if( Meteor.isClient ) {
-  Meteor.startup( function( ) {
-    // code to run on client at startup
-  } );
-    var playerScoreAdd = function( player ) {
-      console.log("playerScoredAdd");
-      console.log( player );
-      Players.update( {_id: player._id}, {$set: {"score": player.score +1}} );
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        $(function(){
+            $(document).on("mouseup", function (){
+                clearTimeout(pointsTimer);
+            });
+        });
+        // code to run on client at startup
+    });
+
+    var pointsTimer, pointsPlayer, timerStart, addTimer;
+
+    var playerScoreAdd = function () {
+        addTimer++;
+        Players.update({_id: pointsPlayer._id}, {$set: {"score": pointsPlayer.score + 1}});
+        pointsTimer = setTimeout(playerScoreAdd, timerStart / addTimer);
     };
-    Template.players.player = function( ) {
-    return Players.find( );
-  };
-  Template.players.events = {
-    "click .playerScoreSubtract": function( event, template ) {
-      Players.update( {_id: this._id}, {$set: {"score": this.score -1}} );
-    },
-    "click .playerScoreAdd": function( event, template ) {
-      //Players.update( {_id: this._id}, {$set: {"score": this.score +1}} );
-    },
-    "mousedown .playerScoreAdd": function( event, template ) {
-      playerScoreAdd(this);
-      //Players.update( {_id: this._id}, {$set: {"score": this.score +1}} );
-    },
-    "mouseup .playerScoreAdd": function( event, template ) {
-      //clearTimeout( t );
-    }
-  };
+    var playerScoreSubtract = function () {
+        addTimer++;
+        Players.update({_id: pointsPlayer._id}, {$set: {"score": pointsPlayer.score - 1}});
+        pointsTimer = setTimeout(playerScoreSubtract, timerStart / addTimer);
+    };
+    Template.players.player = function () {
+        return Players.find();
+    };
+    Template.players.events = {
+        "mousedown .playerScoreSubtract": function (event, template) {
+            addTimer = 1;
+            pointsPlayer = this;
+            timerStart = 1000;
+            playerScoreSubtract();
+        },
+        "mousedown .playerScoreAdd": function (event, template) {
+            addTimer = 1;
+            pointsPlayer = this;
+            timerStart = 1000;
+            playerScoreAdd();
+        }
+    };
 }
-if( Meteor.isServer ) {
-  Meteor.startup( function( ) {
-    // code to run on server at startup
-  } );
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
